ea732210e59e9f71973350e24914a91a135dedb6
kotouharuto <haruto.k.0321@gmail.com>
2025-03-20 15:25:24 +0900

    corsに関するsettings.pyの編集

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sample_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8501",
+    "http://172.16.0.2:8501",
+]  # Streamlit のポートを許可
 
 ROOT_URLCONF = 'app.urls'
 
